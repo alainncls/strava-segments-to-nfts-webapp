eeaab979cd0bf436f8bcf9999a5fc42feef00b08
alainncls <alain.nicolas.perso@gmail.com>
2022-05-31 22:56:05 +0200

    ♻️ Add alt tag on image

--- src/components/StravaLoginButton/StravaLoginButton.tsx
+++ src/components/StravaLoginButton/StravaLoginButton.tsx
@@ -7,7 +7,7 @@ const StravaLoginButton = () => {
       <a
         href={`https://www.strava.com/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&scope=read,read_all,profile:read_all,profile:write,activity:read,activity:read_all,activity:write&redirect_uri=${process.env.REACT_APP_OAUTH_URL}`}
       >
-        <img src={btnStrava} />
+        <img alt={'Strava connect button'} src={btnStrava} />
       </a>
     </div>
   );
