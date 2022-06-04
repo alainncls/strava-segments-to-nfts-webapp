186f777de8eec3fc2ed4bb87f9761fd9c10ee900
Alain Nicolas <alain.nicolas.perso@gmail.com>
2022-06-04 19:06:08 +0200

    ♻️ Test element naming

--- src/components/Loader/Loader.test.tsx
+++ src/components/Loader/Loader.test.tsx
@@ -4,12 +4,12 @@ import Loader from './Loader';
 
 test('renders loading spinner', () => {
   render(<Loader loading={true} />);
-  const navbarElement = screen.getByRole('status');
-  expect(navbarElement).toBeInTheDocument();
+  const spinnerElement = screen.getByRole('status');
+  expect(spinnerElement).toBeInTheDocument();
 });
 
 test('renders no loading spinner', () => {
   render(<Loader loading={false} />);
-  const navbarElement = screen.queryByRole('status');
-  expect(navbarElement).not.toBeInTheDocument();
+  const spinnerElement = screen.queryByRole('status');
+  expect(spinnerElement).not.toBeInTheDocument();
 });
